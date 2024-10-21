9e3bced51648967af9321ad9d4d2c401c4984ddf
Mooreandmoree <131541515+Mooreandmoree@users.noreply.github.com>
2024-10-21 22:46:13 +0100

    modified:   lib/event_manager.rb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -36,3 +36,11 @@ lines.each do |line|
   name = columns[2]
   puts name
 end
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line,index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
