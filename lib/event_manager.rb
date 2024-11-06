86701706e7b76f214a5109b347b0779714111783
Mooreandmoree <131541515+Mooreandmoree@users.noreply.github.com>
2024-11-06 21:38:34 +0100

    modified:   lib/event_manager.rb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -44,3 +44,72 @@ lines.each_with_index do |line,index|
   name = columns[2]
   puts name
 end
+
+lines = File.readlines('event_attendees.csv')
+  lines.each do |line|
+  next if line == ",RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index |line, index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name 
+end
+
+require 'csv'
+puts 'EventManager intialized.'
+
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
+  puts name 
+end
+
+contents = CSV.open(
+  'event_attendees.csv', 
+  headers: true
+  headers_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  puts name 
+end
+
+contents = CSV.open(
+  'event_attendees.csv',
+ headers: true 
+ headers_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than five digits, truncate it to the first five digits
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+# This is a pseudocode showing what the code is meant to do before the actual implementation 
+  puts "#{name} #{zipcode}"
+end 
+
+contents = CSV.open('event_attendees.csv',
+  headers: true 
+  headers_converters: : symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  if zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode > 5
+    zipcode = zipcode[0..4]
+  end
+
+  puts "#{name} #{zipcode}"
+end
